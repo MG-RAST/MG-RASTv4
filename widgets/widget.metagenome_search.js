ed1848b47b3b7b98186d8d198fc9184153960f24
Tobias Paczian <paczian@mcs.anl.gov>
2014-09-09 07:24:38 -0500

    updated to new Retina version

--- widgets/widget.metagenome_search.js
+++ widgets/widget.metagenome_search.js
@@ -1,5 +1,5 @@
 (function () {
-    widget = Retina.Widget.extend({
+    var widget = Retina.Widget.extend({
         about: {
                 title: "Metagenome Search Widget",
                 name: "metagenome_search",
