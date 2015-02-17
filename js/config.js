990598612a1ccbe186b097fd7620309ae12fc208
Tobias Paczian <paczian@mcs.anl.gov>
2015-02-17 08:03:37 -0600

    switched back to production api

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ var RetinaConfig = {
 		       		    "useHeader": true }
 		     },
     "mgrast_ftp": "ftp://ftp.metagenomics.anl.gov",
-    "mgrast_api": "http://dunkirk.mcs.anl.gov/~paczian/MG-RAST/site/CGI/api.cgi", //"http://api.metagenomics.anl.gov",
+    "mgrast_api": "http://api.metagenomics.anl.gov", //"http://dunkirk.mcs.anl.gov/~paczian/MG-RAST/site/CGI/api.cgi",
     "shock_url": "http://shock.metagenomics.anl.gov",
     "awe_url": "http://140.221.67.236:8000",
     "m5nr_solr_url": "http://140.221.67.212:8983/solr/",
