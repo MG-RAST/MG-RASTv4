8a7dc1646373802018f77bb541a0c367bc13f0ac
Tobias Paczian <paczian@mcs.anl.gov>
2015-07-01 06:22:52 -0500

    updated retina added param to config

--- js/config.js
+++ js/config.js
@@ -10,9 +10,10 @@ var RetinaConfig = {
     "mgrast_ftp": "ftp://ftp.metagenomics.anl.gov",
     "mgrast_api": "http://dev.metagenomics.anl.gov/api.cgi",
     "shock_url": "http://shock.metagenomics.anl.gov",
-    "awe_url": "http://140.221.67.236:8000",
+    "awe_url": "http://140.221.76.77:8000",
     "m5nr_solr_url": "http://140.221.67.212:8983/solr/",
     "metagenome_solr_url": "http://140.221.67.239:8983/solr/",
     "authentication": true,
-    "v3BaseLoginUrl": "https://metagenomics.anl.gov/"
+    "v3BaseLoginUrl": "https://metagenomics.anl.gov/",
+    "v3": true
 };
