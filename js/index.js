dc986cf3d983af8d50dfa77a0531b12687b99f3a
Tobias Paczian <paczian@mcs.anl.gov>
2016-03-30 04:44:06 -0500

    updated retina added js of index and main page into js files

---
+++ js/index.js
@@ -0,0 +1,131 @@
+function initWebApp () {
+
+    // check if the browser is compatible
+    var features = { //"async": feature.asynch,
+		     "addEventListener": feature.addEventListener,
+		     "canvas": feature.canvas,
+		     "classList": feature.classList,
+		     "cors": feature.cors,
+		     "css3Dtransform": feature.css3Dtransform,
+		     "cssTransform": feature.cssTransform,
+		     "cssTransition": feature.cssTransition,
+		     "defer": feature.defer,
+		     "localStorage": feature.localStorage,
+		     "matchMedia": feature.matchMedia,
+		     //"pictureElement": feature.pictureElement,
+		     "querySelectorAll": feature.querySelectorAll,
+		     "svg": feature.svg,
+		     "viewportUnit": feature.viewportUnit,
+		     "webGL": feature.webGL
+		   };
+    var allFeaturesAvailable = true;
+    for (var i in features) {
+	if (features.hasOwnProperty(i)) {
+	    if (! features[i]) {
+		console.log(i);
+		allFeaturesAvailable = false;
+	    }
+	}
+    }
+    if (! allFeaturesAvailable) {
+	var html = "<b>Warning</b><p>Your browser does not support all functionality used by this site. Please consider upgrading to a current version.</p><p style='text-align: center;'><a href='http://www.mozilla.org/firefox' target=_blank>Firefox</a> | <a href='http://www.google.com/chrome' target=_blank>Chrome</a> | <a href='http://www.apple.com/safari' target=_blank>Safari</a></p>";
+	document.getElementById('warning').innerHTML = html;
+	document.getElementById('warning').style.display = '';
+    }
+    
+    // keybind for searchbox
+    document.getElementById('searchtext').addEventListener("keypress", function(event){
+	event=event || window.event;
+	if(event.keyCode=='13') {
+	    document.getElementById('searchbutton').click();
+	}
+    });
+    
+    // server status info
+    jQuery.getJSON(RetinaConfig.mgrast_api+"/server/MG-RAST", function (data) {
+	// check if the server is down
+	if (data.status != "ok") {
+	    document.getElementById('searchdiv').style.display = "none";
+	    document.getElementById('error').innerHTML = data.info;
+	    document.getElementById('error').style.display = "";
+	}
+	// check if there is an info message
+	else if (data.info) {
+	    document.getElementById('info').innerHTML = data.info;
+	    document.getElementById('info').style.display = "";
+	}
+	
+	// print server version
+	document.getElementById('version').innerHTML = "version "+data.version;
+	
+	// print server stats
+	var bp = (parseInt(data.basepairs) / 1000000000000).formatString(2);
+	var seq = parseInt(parseInt(data.sequences) / 1000000000).formatString();
+	document.getElementById('load').innerHTML = parseInt(data.metagenomes).formatString()+" metagenomes containing "+seq+" billion sequences and "+bp+" Tbp processed.";
+    });
+    
+    // webapp initialization
+    stm.init({});
+    stm.add_repository({ url: stm.Config.mgrast_api, name: "MG-RAST"});
+    Retina.init({});
+    Retina.load_widget({"name": "login", "resource": "Retina/widgets"}).then( function() {
+	var loginAction = function (data) {
+	    if (data.action != "logout" && data.result == "success") {
+		window.location = 'mgmain.html?mgpage=mydata';
+		if (stm.user && stm.user.hasOwnProperty("preferences")) {
+		    return;
+		}
+		stm.user = data.user;
+		stm.authHeader = { "Authorization": "mgrast "+data.token};
+	    } else {
+		stm.user = null;
+		stm.authHeader = {};
+	    }
+	};
+	Retina.Widget.create('login', { "target": document.getElementById("login_space"),
+		    			"callback": loginAction,
+					"registerEnabled": true,
+					"registerLink": "mgmain.html?mgpage=register",
+					"helpEnabled": true,
+					"helpMenu": [
+					    { "title": "<img src='Retina/images/pencil.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> blog", "url": "http://blog.metagenomics.anl.gov/howto/" },
+					    { "title": "<img src='Retina/images/file-pdf.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> handbook", "url": "ftp://ftp.metagenomics.anl.gov/data/manual/mg-rast-manual.pdf" },
+					    { "title": "<img src='Retina/images/youtube.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> video tutorials", "url": "https://www.youtube.com/channel/UCoBKJ-yYJu5HK1szy3my91A" },
+					    { "title": "<img src='Retina/images/bubbles.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> forum", "url": "https://groups.google.com/forum/#!forum/mg-rast" },
+					    { "title": "<img src='Retina/images/envelop.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'>  feedback", "url": "contact.html" }
+					],
+					"forgotEnabled": true,
+					"forgotLink": "mgmain.html?mgpage=register&forgot=1",
+					"myDataEnabled": true,
+					"myDataLink": "mgmain.html?mgpage=mydata" });
+    });
+    
+    jQuery('.carousel').carousel({"interval": 8000});
+
+    // tell the user we are using cookies
+    window.cookiebar = new jQuery.peekABar();
+    setTimeout(function () {
+	cookiebar.show({
+	    html: 'This site uses cookies to improve user experience. By continuing to use the site you consent to the use of cookies.<button class="btn btn-mini pull-right" onclick="cookiebar.hide();" style="float: right; margin-right: 50px;">OK</button>'
+	});
+    }, 3000);
+
+
+};
+
+// google newsfeed
+function showNewsFeed () {
+    var feed = new google.feeds.Feed("http://press.igsb.anl.gov/mg-rast/feed/");
+    feed.load(function(result) {
+	if (!result.error) {
+	    var html = '<table class="table table-condensed" style="width: 100%; font-size: 12px;">';
+	    for (var i = 0; i < result.feed.entries.length; i++) {
+		var entry = result.feed.entries[i];
+		var date = entry.publishedDate.replace(/^(\w+,\s\d+\s\w+\s\d+).+$/, "$1");
+		html += '<tr><td style="white-space: nowrap;">'+date+'</td><td><a href="'+entry.link+'" target=_blank>'+entry.title+'</a></td></tr>';
+	    }
+	    html += "</table>";
+	    document.getElementById("newsfeed").innerHTML = html;
+	}
+    });
+}
---
+++ js/main.js
@@ -0,0 +1,71 @@
+function initWebApp () {
+    stm.init({});
+    stm.add_repository({ url: stm.Config.mgrast_api, name: "MG-RAST"});
+    Retina.init({});
+    
+    var page  = Retina.cgiParam("mgpage") || "search";
+    
+    changeLocation = function(event, loc) {
+	event = event || window.event;
+	if (event.shiftKey) {
+	    window.open(loc);
+	} else {
+	    window.location = loc;
+	}
+    };
+    Retina.load_widget("metagenome_"+page).then( function() {
+	var mgp = Retina.Widget.create('metagenome_'+page, { "main": document.getElementById("content"),
+							     "sidebar": document.getElementById("sidebar") }, true);
+	
+	Retina.load_widget({"name": "login", "resource": "Retina/widgets"}).then( function() {
+	    var loginAction = function (data) {
+		if (data.action != "logout" && data.result == "success") {
+		    if (stm.user && stm.user.hasOwnProperty("preferences")) {
+			mgp.display({ "main": document.getElementById("content"),
+				      "sidebar": document.getElementById("sidebar") });
+			return;
+		    }
+		    stm.user = data.user;
+		    stm.authHeader = { "Authorization": "mgrast "+data.token};
+		    mgp.display({ "main": document.getElementById("content"),
+				  "sidebar": document.getElementById("sidebar") });
+		} else {
+		    stm.user = null;
+		    stm.authHeader = {};
+		    mgp.display({ "main": document.getElementById("content"),
+				  "sidebar": document.getElementById("sidebar") });
+		}
+	    };
+	    Retina.Widget.create('login', { "target": document.getElementById("login_space"),
+		    			    "callback": loginAction,
+					    "registerEnabled": true,
+					    "registerLink": "mgmain.html?mgpage=register",
+					    "helpEnabled": true,
+					    "helpMenu": [
+						{ "title": "<img src='Retina/images/pencil.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> blog", "url": "http://blog.metagenomics.anl.gov/howto/" },
+						{ "title": "<img src='Retina/images/file-pdf.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> handbook", "url": "ftp://ftp.metagenomics.anl.gov/data/manual/mg-rast-manual.pdf" },
+						{ "title": "<img src='Retina/images/youtube.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> video tutorials", "url": "https://www.youtube.com/channel/UCoBKJ-yYJu5HK1szy3my91A" },
+						{ "title": "<img src='Retina/images/bubbles.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'> forum", "url": "https://groups.google.com/forum/#!forum/mg-rast" },
+						{ "title": "<img src='Retina/images/envelop.png' style='width: 16px; position: relative; bottom: 1px; margin-right: 5px;'>  feedback", "url": "contact.html" }
+					    ],
+					    "forgotEnabled": true,
+					    "forgotLink": "mgmain.html?mgpage=register&forgot=1",
+					    "myDataEnabled": true,
+					    "myDataLink": "mgmain.html?mgpage=mydata" });
+	});
+	Retina.load_widget({"name": "session", "resource": "Retina/widgets"}).then( function() {
+	    Retina.Widget.create('session', { "target": document.getElementById("session_space") });
+	});
+    });
+};
+
+function showNewsFeed () {
+    var feed = new google.feeds.Feed("http://press.igsb.anl.gov/mg-rast/feed/");
+    feed.load(function(result) {
+	if (Retina.WidgetInstances.hasOwnProperty('metagenome_mydata') && document.getElementById('newsfeed')) {
+	    Retina.WidgetInstances.metagenome_mydata[1].showNews(result);
+	} else {
+	    window.newsFeedResult = result;
+	}
+    });
+}
